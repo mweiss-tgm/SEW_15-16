85def071dda602b19fbd141b65e346f96dc7343b
mweiss-tgm <mweiss@student.tgm.ac.at>
2016-04-19 17:23:18 +0200

    Added class TestIstDreieckRechtwinkelig

---
+++ DreieckT3st/test/dreieck/TestIstDreieckRechtwinkelig.java
@@ -0,0 +1,22 @@
+package dreieck;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestIstDreieckRechtwinkelig {
+
+	int seite_a;
+	int seite_b;
+	int seite_c;
+	
+	private Dreieck dreieck;
+	
+	
+	@Before
+	public void setUp() {
+		dreieck = new Dreieck(seite_a,seite_b,seite_c);
+	}
+	
+	
+	
+}
