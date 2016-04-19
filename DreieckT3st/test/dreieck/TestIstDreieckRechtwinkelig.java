42c6680dee33c43b0d6fbe10acd2a5e43be8292e
mweiss-tgm <mweiss@student.tgm.ac.at>
2016-04-19 17:32:19 +0200

    Added methods

--- DreieckT3st/test/dreieck/TestIstDreieckRechtwinkelig.java
+++ DreieckT3st/test/dreieck/TestIstDreieckRechtwinkelig.java
@@ -8,15 +8,74 @@ public class TestIstDreieckRechtwinkelig {
 	int seite_a;
 	int seite_b;
 	int seite_c;
-	
+
 	private Dreieck dreieck;
-	
-	
+
 	@Before
 	public void setUp() {
-		dreieck = new Dreieck(seite_a,seite_b,seite_c);
+		dreieck = new Dreieck(seite_a, seite_b, seite_c);
+	}
+
+	@Test
+	public void testIstKeinDreieck() {
+		dreieck.setSeite_a(0);
+		dreieck.setSeite_b(4);
+		dreieck.setSeite_c(1);
+
+		assertFalse(dreieck.rechtWinkelig());
 	}
 	
-	
-	
+	@Test
+	public void testIstAGroesserBUndC() {
+		dreieck.setSeite_a(10);
+		dreieck.setSeite_b(9);
+		dreieck.setSeite_c(8);
+
+		assertTrue(dreieck.rechtWinkelig());
+	}
+
+	@Test
+	public void testIstBGroesserAUndC() {
+		dreieck.setSeite_a(9);
+		dreieck.setSeite_b(10);
+		dreieck.setSeite_c(8);
+
+		assertTrue(dreieck.rechtWinkelig());
+	}
+
+	@Test
+	public void testIstCGroesserAUndB() {
+		dreieck.setSeite_a(3);
+		dreieck.setSeite_b(5);
+		dreieck.setSeite_c(10);
+
+		assertTrue(dreieck.rechtWinkelig());
+	}
+
+	@Test
+	public void testIstCKleinerB() {
+		dreieck.setSeite_a(5);
+		dreieck.setSeite_b(5);
+		dreieck.setSeite_c(4);
+
+		assertFalse(dreieck.rechtWinkelig());
+	}
+
+	@Test
+	public void testIstNichtRechtWinkelig() {
+		dreieck.setSeite_a(3);
+		dreieck.setSeite_b(3);
+		dreieck.setSeite_c(4);
+
+		assertFalse(dreieck.rechtWinkelig());
+	}
+
+	@Test
+	public void testIstCGroesserBKleinerA() {
+		dreieck.setSeite_a(6);
+		dreieck.setSeite_b(3);
+		dreieck.setSeite_c(6);
+
+		assertFalse(dreieck.rechtWinkelig());
+	}
 }
