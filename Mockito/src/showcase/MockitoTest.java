2c0b54892c56a55d0e1908c7d3732dbd9a91cdf5
mweiss-tgm <mweiss@student.tgm.ac.at>
2016-04-20 09:39:26 +0200

    Initial commit

---
+++ Mockito/src/showcase/MockitoTest.java
@@ -0,0 +1,108 @@
+package showcase;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.*;
+import org.mockito.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MockitoTest {
+	
+	LinkedList mockedList;
+	
+	@Before
+	public void setUp() {
+		mockedList = mock(LinkedList.class);
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public void testBehaviour() {
+
+		 //mock creation
+		 List mockedList = mock(List.class);
+
+		 //using mock object
+		 mockedList.add("one");
+		 mockedList.clear();
+
+		 //verification
+		 verify(mockedList).add("one");
+		 verify(mockedList).clear();
+	}
+	
+	@Test(expected=RuntimeException.class)
+	public void testStubbing() {
+		 //You can mock concrete classes, not just interfaces
+		 LinkedList mockedList = mock(LinkedList.class);
+
+		 //stubbing
+		 when(mockedList.get(0)).thenReturn("first");
+		 when(mockedList.get(1)).thenThrow(new RuntimeException());
+
+		 //following prints "first"
+		 System.out.println(mockedList.get(0));
+
+		 //following throws runtime exception
+		 System.out.println(mockedList.get(1));
+
+		 //following prints "null" because get(999) was not stubbed
+		 System.out.println(mockedList.get(999));
+
+		 //Although it is possible to verify a stubbed invocation, usually it's just redundant
+		 //If your code cares what get(0) returns, then something else breaks (often even before verify() gets executed).
+		 //If your code doesn't care what get(0) returns, then it should not be stubbed. Not convinced? See here.
+		 verify(mockedList).get(0);
+	}
+	
+	@Test
+	public void testArgumentMatcher() {
+		 //stubbing using built-in anyInt() argument matcher
+		 when(mockedList.get(anyInt())).thenReturn("element");
+
+		 //stubbing using custom matcher (let's say isValid() returns your own matcher implementation):
+		// when(mockedList.contains(argThat(isValid()))).thenReturn("element");
+
+		 //following prints "element"
+		 System.out.println(mockedList.get(999));
+
+		 //you can also verify using an argument matcher
+		 verify(mockedList).get(anyInt());
+	}
+	
+	@Test
+	public void testNumbersOfInvocations() {
+		//using mock
+		 mockedList.add("once");
+
+		 mockedList.add("twice");
+		 mockedList.add("twice");
+
+		 mockedList.add("three times");
+		 mockedList.add("three times");
+		 mockedList.add("three times");
+
+		 //following two verifications work exactly the same - times(1) is used by default
+		 verify(mockedList).add("once");
+		 verify(mockedList, times(1)).add("once");
+
+		 //exact number of invocations verification
+		 verify(mockedList, times(2)).add("twice");
+		 verify(mockedList, times(3)).add("three times");
+
+		 //verification using never(). never() is an alias to times(0)
+		 verify(mockedList, never()).add("never happened");
+
+		 //verification using atLeast()/atMost()
+		 verify(mockedList, atLeastOnce()).add("three times");
+		 verify(mockedList, atLeast(2)).add("twice");
+		 verify(mockedList, atMost(5)).add("three times");
+
+	}
+}
